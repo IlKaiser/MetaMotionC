af99f74b57621755f2f7cd42335f3b9b19d34020
IlKaiser <samuele.rinaldi90@gmail.com>
2020-09-30 22:29:48 +0200

    Sensor Fusion bringup

---
+++ Python/MetaWear-SDK-Python/custom/sensor_fusion.py
@@ -0,0 +1,64 @@
+# usage: python stream_acc.py [mac1] [mac2] ... [mac(n)]
+from __future__ import print_function
+from mbientlab.metawear import *
+from mbientlab.metawear.cbindings import *
+from time import sleep
+from threading import Event
+
+import platform
+import sys
+import copy
+
+if sys.version_info[0] == 2:
+    range = xrange
+
+class State:
+    def __init__(self, device):
+        self.device = device
+        self.samples = 0
+        self.callback = FnVoid_VoidP_DataP(self.data_handler)
+        self.gyro_data = []
+
+    def data_handler(self, ctx, data):
+        parsed_data=parse_value(data)
+        print("%s -> %s" % (self.device.address, parsed_data))
+        self.gyro_data.append(copy.deepcopy(parsed_data))
+        self.samples+= 1
+
+states = []
+for i in range(len(sys.argv) - 1):
+    d = MetaWear(sys.argv[i + 1])
+    d.connect()
+    print("Connected to " + d.address)
+    states.append(State(d))
+
+for s in states:
+    print("Configuring device")
+    libmetawear.mbl_mw_settings_set_connection_parameters(s.device.board, 7.5, 7.5, 0, 6000)
+    sleep(1.5)
+
+    signal_corrected_gyro = libmetawear.mbl_mw_sensor_fusion_get_data_signal(s.device.board,SensorFusionData.CORRECTED_GYRO)
+    libmetawear.mbl_mw_datasignal_subscribe(signal_corrected_gyro, None, s.callback)
+    libmetawear.mbl_mw_sensor_fusion_enable_data(s.device.board, SensorFusionData.CORRECTED_GYRO)
+
+    libmetawear.mbl_mw_sensor_fusion_set_acc_range(s.device.board,SensorFusionAccRange._16G)
+    libmetawear.mbl_mw_sensor_fusion_set_gyro_range(s.device.board,SensorFusionGyroRange._1000DPS)
+    libmetawear.mbl_mw_sensor_fusion_set_mode(s.device.board,SensorFusionMode.NDOF)
+    libmetawear.mbl_mw_sensor_fusion_write_config(s.device.board)
+
+    libmetawear.mbl_mw_sensor_fusion_start(s.device.board)
+
+sleep(5.0)
+
+for s in states:
+    libmetawear.mbl_mw_sensor_fusion_stop(s.device.board)
+    libmetawear.mbl_mw_sensor_fusion_clear_enabled_mask(s.device.board)
+    libmetawear.mbl_mw_datasignal_unsubscribe(signal_corrected_gyro)
+    libmetawear.mbl_mw_debug_disconnect(s.device.board)
+print("Total Samples Received")
+for s in states:
+    print("%s -> %d" % (s.device.address, s.samples))
+print("Gyro again just to be sure")
+for s in states:
+    for g in s.gyro_data:
+        print("%s -> %s" % (s.device.address, g))
